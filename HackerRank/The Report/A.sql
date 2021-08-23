eacba07ca92fcd2165697a77c8baaf844cb301b0
Teja Goud Kandula <kandulateja6@gmail.com>
2021-08-23 07:56:39 +0530

    Update HackerRank The Report A.sql

--- HackerRank/The Report/A.sql
+++ HackerRank/The Report/A.sql
@@ -16,5 +16,8 @@ ORDER BY g.grade DESC,
          s.marks
 
 /*
-  The trick here is that the answer requires to use the join condition based on BETWEEN clause 
+  The trick here is that the answer requires to use the join condition based on BETWEEN clause
+
+  We can't use UNION here because that would require 2 select clauses and order by should be used on the each of the select clauses.
+  When using UNION / UNION ALL one should use order by only on the last select clause, so we can't achieve the required output using UNION and 2 select clauses.
 */
