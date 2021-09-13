d9082e0ea0a6f9cb405a770ac65668eb2e16b63b
Teja Goud Kandula <kandulateja6@gmail.com>
2021-09-13 22:37:26 +0530

    LeetCode Reformart Department Table Q

---
+++ LeetCode/Reformat Department Table/Q.sql
@@ -0,0 +1,15 @@
+-- Mysql code
+
+create table Department
+(
+	id int,
+    revenue  int,
+	month char(10)
+);
+
+insert into Department values
+( 1,     8000    , 'Jan'),
+( 2,     9000    , 'Jan'),
+( 3,     10000   , 'Feb'),
+( 1,     7000    , 'Feb'),
+( 1,     6000    , 'Mar');
